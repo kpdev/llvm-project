de4a15be00cea7787fa13fdef8e45e21d1703eaa
Pavel Kosov <kpdev42@gmail.com>
2023-02-01 00:32:31 +0300

    [PP-EXT] Adopt for LLVM v15.0.4

--- clang/lib/Parse/ParseDecl.cpp
+++ clang/lib/Parse/ParseDecl.cpp
@@ -4769,7 +4769,9 @@ void Parser::ParseStructDeclaration(
   }
 }
 
-bool Parser::TryParsePPExt(Decl *TagDecl, SmallVector<Decl *, 32>& FieldDecls) {
+bool Parser::TryParsePPExt(Decl *TagDecl,
+                           SmallVector<Decl *, 32>& FieldDecls,
+                           const ParsedAttributes& Attrs) {
   if (Tok.isNot(clang::tok::l_square)) {
     return false;
   }
@@ -4797,7 +4799,7 @@ bool Parser::TryParsePPExt(Decl *TagDecl, SmallVector<Decl *, 32>& FieldDecls) {
     const char *PrevSpec = nullptr;
     bool isInvalid = DS.SetTypeSpecType(FieldType, Loc, PrevSpec, DiagID, Policy);
     assert(!isInvalid);
-    ParsingFieldDeclarator DeclaratorInfo(*this, DS);
+    ParsingFieldDeclarator DeclaratorInfo(*this, DS, Attrs);
     SourceLocation CommaLoc;
     DeclaratorInfo.D.setCommaLoc(CommaLoc);
     auto ID = PP.getIdentifierInfo(FieldName);
@@ -5010,8 +5012,6 @@ void Parser::ParseStructUnionBody(SourceLocation RecordLoc,
 
   T.consumeClose();
 
-  bool isPPExtParsed = TryParsePPExt(TagDecl, FieldDecls);
-
   ParsedAttributes attrs(AttrFactory);
   // If attributes exist after struct contents, parse them.
   MaybeParseGNUAttributes(attrs, &LateFieldAttrs);
@@ -5020,6 +5020,8 @@ void Parser::ParseStructUnionBody(SourceLocation RecordLoc,
   ParseLexedCAttributeList(LateFieldAttrs, /*EnterScope=*/false);
 
   SmallVector<Decl *, 32> FieldDecls(TagDecl->fields());
+
+  bool isPPExtParsed = TryParsePPExt(TagDecl, FieldDecls, attrs);
 
   Actions.ActOnFields(getCurScope(), RecordLoc, TagDecl, FieldDecls,
                       T.getOpenLocation(), T.getCloseLocation(), attrs);
