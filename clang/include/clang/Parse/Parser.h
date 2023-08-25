e1a35f7a423fc153ee3b2f111389870b93667142
Pavel Kosov <kpdev42@gmail.com>
2023-08-25 10:59:17 +0300

    [PP-EXT] Mangle multimethods names

--- clang/include/clang/Parse/Parser.h
+++ clang/include/clang/Parse/Parser.h
@@ -1557,7 +1557,8 @@ private:
   enum class PPFuncMode {
     Ctor,
     Init,
-    Increment
+    Increment,
+    MMDefault
   };
 
   struct PPMangledNames {
@@ -1577,7 +1578,12 @@ private:
 
     void addVariantName(std::string VariantName);
 
-    __attribute__((noinline))
+    std::string MMName;
+    std::string MMArrayName;
+
+    void setMMName(std::string Name);
+
+    LLVM_ATTRIBUTE_NOINLINE
     void dump();
   };
 
@@ -1591,7 +1597,8 @@ private:
   void AddFunc(std::string FuncName,
                PPFuncMode Mode,
                std::string TagNameToInit,
-               PPMangledNames& ppMNames);
+               PPMangledNames& ppMNames,
+               SmallVector<DeclaratorChunk::ParamInfo, 16> *ParamInfo = nullptr);
 
   void FieldGenerator(const char* FieldName,
                       DeclSpec::TST FieldType,
