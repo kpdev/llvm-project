a2141bb2d91a716c90b43cb707c04f0a7b87bef4
Pavel Kosov <kpdev42@gmail.com>
2024-01-15 00:08:03 +0300

    [PP-EXT] Add generation of debug output to stdout

--- clang/lib/CodeGen/CodeGenModule.h
+++ clang/lib/CodeGen/CodeGenModule.h
@@ -1879,6 +1879,9 @@ private:
                       llvm::Value* AllocatedBytes,
                       llvm::Value* HandlersArray,
                       llvm::Value* DefaultHandler);
+  llvm::CallInst* CreateCallPrintf(llvm::BasicBlock* BB,
+                                   StringRef FormatStr,
+                                   llvm::Value* Arg);
 
   void EmitGlobalVarDefinition(const VarDecl *D, bool IsTentative = false);
   void EmitAliasDefinition(GlobalDecl GD);
