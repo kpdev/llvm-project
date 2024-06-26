0c3bd463298b9dc8ee0b619284625f3f367e45ea
Pavel Kosov <kpdev42@gmail.com>
2024-06-26 08:58:12 +0300

    [PP-EXT] Add support for pointer types in tagged specializations

--- clang/include/clang/Parse/Parser.h
+++ clang/include/clang/Parse/Parser.h
@@ -1575,7 +1575,8 @@ private:
 
   struct SpecsDescr {
     std::string VariantName;
-    IdentifierInfo* FullNameIInfo;
+    IdentifierInfo* FullNameIInfo = nullptr;
+    bool IsPtr = false;
   };
   using SpecsVec = SmallVector<SpecsDescr>;
   Optional<SpecsVec> TryParsePPExt(Decl *TagDecl,
