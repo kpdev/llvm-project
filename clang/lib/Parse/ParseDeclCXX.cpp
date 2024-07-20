a24c6a03568489a44eba2510aae83d526860e2e7
Pavel Kosov <kpdev42@gmail.com>
2024-07-20 04:49:36 +0300

    [PP-EXT] Allow (tagged) generalization extension with ptrs

--- clang/lib/Parse/ParseDeclCXX.cpp
+++ clang/lib/Parse/ParseDeclCXX.cpp
@@ -2098,10 +2098,11 @@ void Parser::ParseClassSpecifier(tok::TokenKind TagTokKind,
         ParsedAttributes TestAttrs(AttrFactory);
         auto VariantName = Tok.getIdentifierInfo()->getName().str();
         auto VariantNameIdentifier = &PP.getIdentifierTable().get(VariantName);
+        const bool IsPtr = NextToken().is(tok::star);
         auto TestNameStr = std::string("__pp_struct_") + Name->getName().str()
                             + "__"
                             + (TagName.empty() ?
-                                VariantName :
+                                VariantName + (IsPtr ? "_pp_ptr" : "") :
                                 TagName.str());
         auto TestName = &PP.getIdentifierTable().get(TestNameStr);
 
@@ -2151,7 +2152,7 @@ void Parser::ParseClassSpecifier(tok::TokenKind TagTokKind,
                         TestAttrs, TestDecl, FieldDecls);
         FieldGenerator("__pp_tail",
                        FieldType,
-                       VariantRecordDecl, false,
+                       VariantRecordDecl, IsPtr,
                        TestAttrs, TestDecl, FieldDecls);
         SmallVector<Decl *, 32> TestFieldDecls(cast<RecordDecl>(TestDecl)->fields());
         Actions.ActOnFields(getCurScope(), CurLoc, TestDecl, TestFieldDecls,
@@ -2181,6 +2182,9 @@ void Parser::ParseClassSpecifier(tok::TokenKind TagTokKind,
         }
       }
       ConsumeToken();
+      if (Tok.is(tok::star)) {
+        ConsumeToken();
+      }
       assert(Tok.is(tok::semi));
       ConsumeToken();
       assert(Tok.is(tok::greater));
