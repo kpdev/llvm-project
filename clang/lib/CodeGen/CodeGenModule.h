d8bf2f5ac845d749b41272078af98d44964e2f20
Pavel Kosov <kpdev42@gmail.com>
2024-11-10 17:14:50 +0300

    [PP-EXT] Add initializer for array of create_spec

--- clang/lib/CodeGen/CodeGenModule.h
+++ clang/lib/CodeGen/CodeGenModule.h
@@ -1878,6 +1878,7 @@ private:
   using MMParams = std::vector<FunctionDecl::PPMMParam>;
   void AddPPSpecialization(llvm::Function* F,
                            const MMParams& Gens);
+  void PPExtInitCreateSpecArray(StringRef GenName, llvm::Module& Parent);
   llvm::Function* PPExtCreateMMRecorder(llvm::Function* BaseF);
   llvm::Function* ExtractDefaultPPMMImplementation(llvm::Function* F,
                                                    const clang::FunctionDecl* FD);
