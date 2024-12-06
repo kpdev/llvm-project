366eacc0fb180bb90d00053d5905c969a5c52230
Pavel Kosov <kpdev42@gmail.com>
2024-12-06 03:02:13 +0300

    [PP-EXT] Add support for init direct fields in generalizations

--- clang/include/clang/Parse/Parser.h
+++ clang/include/clang/Parse/Parser.h
@@ -1572,7 +1572,30 @@ private:
     bool NeedToAddLParen = true
   );
 
+  enum class PPStructType {
+    Default,
+    Generalization,
+    Specialization
+  };
+
+  PPStructType PPExtGetStructType(const RecordDecl* RD) const;
+
+  struct PPStructInitDesc {
+    NamedDecl* VD;
+    const RecordDecl* RD;
+    const PPStructType Type;
+  };
+
+  std::vector<PPStructInitDesc> PPExtGetRDListToInit(const RecordDecl* RD) const;
+
   std::string PPExtConstructTagName(StringRef GenName);
+
+  struct PPMemberInitData {
+    Expr* Assign;
+    Expr* MemberAccess;
+  };
+
+  PPMemberInitData PPExtInitPPStruct(PPStructInitDesc IDesc, Expr* MemberAccess);
 
   static DeclSpec::TST PPExtGetFieldTypeByTokKind(tok::TokenKind TK);
 
